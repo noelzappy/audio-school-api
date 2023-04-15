2cb4e68764158fc4212a29161eea00124fb177af
Emmanuel Yeboah <noelzappy@gmail.com>
2023-04-15 19:07:06 +0000

    feat: introduced controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,2 +1,8 @@
 module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
+module.exports.categoryController = require('./category.controller');
+module.exports.gradeController = require('./grade.controller');
+module.exports.chapterController = require('./chapter.controller');
+module.exports.bookController = require('./book.controller');
+module.exports.chapterPlayController = require('./chapterPlay.controller');
+module.exports.commentController = require('./comment.controller');
